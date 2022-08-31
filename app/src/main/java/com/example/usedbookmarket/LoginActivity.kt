675b5a413e0c99f0a53eb1c1ac6a590f6a7c0ab8
HojinLim <69897998+HojinLim@users.noreply.github.com>
2022-08-31 13:42:58 +0900

    회원가입, 로그인 이슈

--- app/src/main/java/com/example/usedbookmarket/LoginActivity.kt
+++ app/src/main/java/com/example/usedbookmarket/LoginActivity.kt
@@ -3,9 +3,14 @@ package com.example.usedbookmarket
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -14,6 +19,38 @@ class LoginActivity : AppCompatActivity() {
         findViewById<AppCompatButton>(R.id.goSignUpButton).setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
-        //TODO 로그인 구현
+        initLoginButton()
+
+    }
+    // TODO 로그인 구현
+    private fun initLoginButton(){
+        val loginButton= findViewById<AppCompatButton>(R.id.loginButton)
+        loginButton.setOnClickListener{
+            val email= getInputEmail()
+            val password= getInputPassword()
+
+            if(email== null && password == null) {
+                Log.d("hi", "둘다 비어있어")
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){ task ->
+                    if(task.isSuccessful){
+                        //finish()
+                        startActivity(Intent(this, StartActivity::class.java))
+                        Toast.makeText(this, "로그인 완료!", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this, "잘못 입력하였거나 계정이 존재하지 않습니다!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    private fun getInputEmail(): String{
+        return findViewById<EditText>(R.id.inputEmailEditText).text.toString()
+    }
+    private fun getInputPassword(): String{
+        return findViewById<EditText>(R.id.inputPasswordEditText).text.toString()
     }
 }
--- app/src/main/java/com/example/usedbookmarket/SignUpActivity.kt
+++ app/src/main/java/com/example/usedbookmarket/SignUpActivity.kt
@@ -2,10 +2,42 @@ package com.example.usedbookmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        auth= Firebase.auth
+        initSignUpButton()
+    }
+    private fun initSignUpButton(){
+        val signUpButton= findViewById<AppCompatButton>(R.id.signUpButton)
+        signUpButton.setOnClickListener {
+            val email= getInputEmail()
+            val password= getInputPassword()
+            auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this){ task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "회원가입에 성공하였습니다! 로그인 버튼을 눌러 로그인해주세요.", Toast.LENGTH_SHORT).show()
+                        // finish()
+                    }else {
+                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+    private fun getInputEmail(): String{
+        return findViewById<EditText>(R.id.inputEmailEditText).text.toString()
+    }
+    private fun getInputPassword(): String{
+        return findViewById<EditText>(R.id.inputPasswordEditText).text.toString()
     }
 }
