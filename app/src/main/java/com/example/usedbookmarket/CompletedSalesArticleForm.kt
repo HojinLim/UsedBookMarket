e25a1101014623b5ab46c7aa9d188b014a6505b7
Hojin <hojinim@gmail.com>
2023-02-24 18:54:13 +0900

    자신의 글이 아닐시 수정하기 불가

--- app/src/main/java/com/example/usedbookmarket/CompletedSalesArticleForm.kt
+++ app/src/main/java/com/example/usedbookmarket/CompletedSalesArticleForm.kt
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.example.usedbookmarket.model.ArticleForm
 import com.google.firebase.auth.FirebaseAuth
@@ -34,6 +36,10 @@ class CompletedSalesArticleForm : AppCompatActivity() {
         val formModel = intent.getParcelableExtra<ArticleForm>("formModel")
         formModel ?: return
 
+        // 자신이 작성한 글이 아닐 시 수정 불가
+        if(formModel.uid != auth.currentUser?.uid){
+            findViewById<Button>(R.id.article_form_complete_btn).isVisible= false
+        }
 
         val coverImageView = findViewById<ImageView>(R.id.c_sales_article_form_coverImg)
         findViewById<TextView>(R.id.c_sales_article_form_detail_title).text = formModel?.title.orEmpty()
